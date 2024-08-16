0d7da0dbc7404276fce01bbc1791b4c2751cf243
pimentellima <matheus_dere@hotmail.com>
2024-08-16 14:16:14 -0300

    fix: corrige imagem de perfil distorcida

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 export default function UserAvatar({ imageUrl }: { imageUrl?: string }) {
     return (
         <Avatar className="h-20 w-20 sm:h-28 sm:w-28 ring-primary">
-            <AvatarImage src={imageUrl} alt="Avatar image" />
+            <AvatarImage className='object-cover' src={imageUrl} alt="Avatar image" />
             <AvatarFallback>
                 <UserIcon className="h-24 w-24 sm:h-28 sm:w-28 p-3 z-10" />
             </AvatarFallback>
